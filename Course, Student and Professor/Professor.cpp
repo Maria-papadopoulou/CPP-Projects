8a0140c14ab2c6908c013dedddfb5d8c9340fcfb
Μαρία Παπαδοπούλου <papadopoyloy.mary@hotmail.com>
2023-05-30 19:39:12 +0300

    Delete Course, Student and Professor directory

--- Course, Student and Professor/Professor.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Student.h"
-
-void Professor::Show ()
-{
-    std::cout << "Printing Professor" << std::endl;
-}
-
--- Course, Student and Professor/Student.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Student.h"
-
-
-void Student::Show ()
-{
-    std::cout << "Printing Student..." << std::endl;
-}
